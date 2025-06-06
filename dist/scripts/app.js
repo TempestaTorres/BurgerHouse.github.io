dcc4f63c3a3d6d4e2e90b5ef1ec84a95417f575f
TempestaTorres <142347458+TempestaTorres@users.noreply.github.com>
2025-06-06 15:16:00 +0300

    animation added

--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -2,15 +2,48 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const bgLeft = document.querySelector("#bg-paper-left");
     const bgRight = document.querySelector("#bg-paper-right");
+    const header = document.querySelector('.header');
+    const ScrollItems = document.querySelectorAll('.slide-up');
+    const formContainer = document.querySelector('.form-container');
+    const form = document.querySelector('#form');
 
     let scrollX;
     let scrollY;
 
+    function scrollObserver(){
+        ScrollItems.forEach(Item => {
+
+            if (isPartiallyVisible(Item)) {
+                Item.classList.add('slide-up-active');
+            }
+            else if (Item.classList.contains('slide-up-active')) {
+                Item.classList.remove('slide-up-active');
+            }
+         });
+        if (isPartiallyVisible(formContainer)) {
+            formContainer.classList.add('form-container-active');
+        }
+        else if (formContainer.classList.contains('form-container-active')) {
+            formContainer.classList.remove('form-container-active');
+        }
+
+    }
+    function headerObserver() {
+
+        if (window.scrollY > 0 && !header.classList.contains('header-active')) {
+            header.classList.add('header-active');
+        }
+        else if (window.scrollY <= 0 && header.classList.contains('header-active')) {
+            header.classList.remove('header-active');
+        }
+    }
     function scrollHandler() {
 
         scrollX = window.scrollX;
         scrollY = window.scrollY;
 
+        headerObserver(scrollY);
+        scrollObserver();
         elTranslate(bgLeft,0, scrollY * 0.01);
         elTranslate(bgRight,0, scrollY * -0.1);
 
